a4608be0d3261adc522fac302069d104257b2967
muxueqz <zhangmingyuan240@gmail.com>
2022-07-27 15:25:38 +0800

    move colorscheme config

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -4,3 +4,8 @@ local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   return
 end
+require('onedark').setup {
+  -- style = 'light',
+  -- toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+  toggle_style_list = { 'dark', 'light' }, -- List of styles to toggle between
+}
