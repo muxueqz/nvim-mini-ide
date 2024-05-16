2aca741f9a787aa096a498e159cb292f2bb6eb3b
muxueqz <zhangmingyuan240@gmail.com>
2024-05-16 14:55:58 +0800

    fix previm open is not working in jail

--- lua/plugins/previm.lua
+++ lua/plugins/previm.lua
@@ -4,17 +4,9 @@ return {
   event = "VeryLazy",
   ft = "markdown",
   config = function()
-    vim.g.previm_open_cmd = "xdg-open"
+    -- vim.g.previm_open_cmd = "xdg-open"
+    vim.g.previm_open_cmd = "x-www-browser"
     vim.g.previm_custom_preview_base_dir = "/dev/shm/previm_cache/"
-    -- vim.api.nvim_set_keymap("n", "<Space>mp", ":PrevimOpen<cr>", { silent = true })
-    -- vim.keymap.set("n", "<leader>mp", ":PrevimOpen<cr>", { silent = true })
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>lp"] = { "<cmd>PrevimOpen<cr>", "Preview markdown" },
-    })
-    -- vim.keymap.set("n", "<leader>Tm", "<cmd>PrevimOpen<cr>", { silent = true })
-    --     -- vim.cmd [[
-    --     --   nmap <Space>mp <Plug>MarkdownPreview
-    --     -- ]]
+    vim.keymap.set("n", "<leader>cp", ":PrevimOpen<cr>", { desc = "Preview Markdown", silent = true })
   end,
 }
